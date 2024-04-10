5b96232a61ea6f613c7abb1641575269060e0f07
ObviousSloth <68105844+ObviousSloth@users.noreply.github.com>
2024-04-10 18:14:18 +0200

    Update Ocelot
    ApiGateway now reroutes microservices correctly

--- InstantaneousGram-ApiGateway/Program.cs
+++ InstantaneousGram-ApiGateway/Program.cs
@@ -4,12 +4,13 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseOcelot().Wait();
+await app.UseOcelot();
 
 app.Run();
---
+++ InstantaneousGram-Authentication/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstantaneousGram_Login.Controllers
+{
+    [Route("api/Value")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<Value>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
