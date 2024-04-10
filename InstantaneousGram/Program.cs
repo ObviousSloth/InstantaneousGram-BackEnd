6c86429db897f60cc4c0b59f0fa321ccbddeeaef
ObviousSloth <68105844+ObviousSloth@users.noreply.github.com>
2024-04-10 03:35:14 +0200

    try to set up api gateway

--- InstantaneousGram/Program.cs
+++ InstantaneousGram/Program.cs
@@ -1,15 +1,12 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseOcelot().Wait();
 
 app.Run();
