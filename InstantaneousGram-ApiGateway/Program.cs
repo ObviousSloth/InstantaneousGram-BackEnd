8a6297ab5c06ae1ae7976a44d1bd2967f919e026
ObviousSloth <429428@student.fontys.nl>
2024-06-06 15:50:20 +0200

    update filles

--- InstantaneousGram-ApiGateway/Program.cs
+++ InstantaneousGram-ApiGateway/Program.cs
@@ -3,13 +3,12 @@ using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Kubernetes;
+using k8s;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// Add Cors
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyPolicy",
@@ -23,16 +22,32 @@ builder.Services.AddCors(options =>
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Kubernetes client configuration
+builder.Services.AddSingleton<IKubernetes>(sp =>
+{
+    var config = KubernetesClientConfiguration.IsInCluster()
+        ? KubernetesClientConfiguration.InClusterConfig()
+        : KubernetesClientConfiguration.BuildConfigFromConfigFile();
+    return new Kubernetes(config);
+});
 
 
 // Add Ocelot with Kubernetes provider
 builder.Services.AddOcelot(builder.Configuration)
                 .AddKubernetes();
-                
+
 var app = builder.Build();
+
+var kubernetesClient = app.Services.GetRequiredService<IKubernetes>();
+var services = await kubernetesClient.CoreV1.ListNamespacedServiceAsync("instantaneousgram");
+foreach (var service in services.Items)
+{
+    Console.WriteLine($"Service: {service.Metadata.Name}");
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
