9b9790f693a39e6557bca615bf0ca8b68907826d
ObviousSloth <429428@student.fontys.nl>
2024-06-10 18:52:13 +0200

    sad stuff

--- InstantaneousGram-ApiGateway/Program.cs
+++ InstantaneousGram-ApiGateway/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Kubernetes;
-using k8s;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,28 +24,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add Kubernetes client configuration
-builder.Services.AddSingleton<IKubernetes>(sp =>
-{
-    var config = KubernetesClientConfiguration.IsInCluster()
-        ? KubernetesClientConfiguration.InClusterConfig()
-        : KubernetesClientConfiguration.BuildConfigFromConfigFile();
-    return new Kubernetes(config);
-});
+
 
 
 // Add Ocelot with Kubernetes provider
-builder.Services.AddOcelot(builder.Configuration)
-                .AddKubernetes();
+builder.Services.AddOcelot(builder.Configuration).AddKubernetes(); // Add Kubernetes provider
+
+ 
 
 var app = builder.Build();
 
-var kubernetesClient = app.Services.GetRequiredService<IKubernetes>();
-var services = await kubernetesClient.CoreV1.ListNamespacedServiceAsync("instantaneousgram");
-foreach (var service in services.Items)
-{
-    Console.WriteLine($"Service: {service.Metadata.Name}");
-}
 
 
 // Configure the HTTP request pipeline.
