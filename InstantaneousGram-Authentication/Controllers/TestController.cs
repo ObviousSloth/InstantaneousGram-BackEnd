b087258ea09e583e7848fa38ae0c377d701cb522
ObviousSloth <68105844+ObviousSloth@users.noreply.github.com>
2024-06-12 01:29:02 +0200

    added authentication support

--- InstantaneousGram-Authentication/Controllers/TestController.cs
+++ InstantaneousGram-Authentication/Controllers/TestController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace InstantaneousGram_ApiGateway.Controllers
+namespace InstantaneousGram_Authentication.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
