0f695d16a0911caafb189ecb5a272f9ed8fea8b0
ObviousSloth <68105844+ObviousSloth@users.noreply.github.com>
2024-06-24 02:47:10 +0200

    added scalling for loadtests

--- MiniKube/LoadTests/load-test.js
+++ MiniKube/LoadTests/load-test.js
@@ -3,14 +3,7 @@ import { sleep, check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 10 },   // ramp up to 10 users over 2 minutes
-    { duration: '5m', target: 10 },   // stay at 10 users for 5 minutes
-    { duration: '2m', target: 50 },   // ramp up to 50 users over 2 minutes
-    { duration: '5m', target: 50 },   // stay at 50 users for 5 minutes
-    { duration: '2m', target: 100 },  // ramp up to 100 users over 2 minutes
-    { duration: '5m', target: 100 },  // stay at 100 users for 5 minutes
-    { duration: '2m', target: 200 },  // ramp up to 200 users over 2 minutes
-    { duration: '5m', target: 200 },  // stay at 200 users for 5 minutes
+
     { duration: '2m', target: 300 },  // ramp up to 300 users over 2 minutes
     { duration: '5m', target: 300 },  // stay at 300 users for 5 minutes
     { duration: '2m', target: 400 },  // ramp up to 400 users over 2 minutes
@@ -19,10 +12,11 @@ export let options = {
     { duration: '10m', target: 500 }, // stay at 500 users for 10 minutes
     { duration: '2m', target: 0 },    // ramp down to 0 users over 2 minutes
   ],
+  insecureSkipTLSVerify: true, // Globally disable TLS verification
 };
 
 export default function () {
-  let res = http.get('http://instantaneousgram.ddns.net/userprofile/api/test');
+  let res = http.get('https://instantaneousgram.ddns.net/userprofile/api/test');
   check(res, {
     'status was 200': (r) => r.status == 200,
     'response time was <= 200ms': (r) => r.timings.duration <= 200,
